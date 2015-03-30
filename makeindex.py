0bcefe7d0126608cd25af9cf2c2c59e562be6b20
analog <matt.poepping@seagate.com>
2015-03-30 15:39:36 -0600

    working with multiple directories better

--- makeindex.py
+++ makeindex.py
@@ -12,48 +12,54 @@ import sys
 
 config = ConfigParser.SafeConfigParser()
 
-if os.path.isfile("makeindex.cfg"):
-  config.read('makeindex.cfg')
-  C_HTMLPATH           = config.get('main', 'htmlPath')
-  C_TEMPLATE_FILE      = config.get('main','template')
-  C_INDEXTEMPLATE_FILE = config.get('main','indextemplate')
-  C_TEMPLATE           = open(C_TEMPLATE_FILE).read()
-  C_INDEXTEMPLATE      = open(C_INDEXTEMPLATE_FILE).read()
-  C_UNPROCESSED        = config.get('main','unprocessed')
-  C_PROCESSED          = config.get('main','processed')
-  C_SUBFOLDERS         = config.get('main', 'subfolders').split(',')
-  C_DEBUG              = config.getboolean('main','debug')
-
-# stage one render all the htm files into html files
-for folder in [C_HTMLPATH] + C_SUBFOLDERS:
-  if C_DEBUG: print folder
-  for filename in glob.glob( folder + "/*." + C_UNPROCESSED):
-    if C_DEBUG: print(filename)
+# for each page in the path, render it with the correct template
+def renderPages(path, C_TEMPLATE, C_UNPROCESSED  ):
+  for filename in glob.glob( path + "/*." + C_UNPROCESSED):
     data = {'content': unicode(file( filename ).read(), "utf-8", errors="ignore")}
     with open( filename + "l" , "w") as f:
       f.write(  pystache.render( C_TEMPLATE, data ) )
 
-  #os.unlink( item['path'] )
+def createDateIndex(C_HTMLPATH, C_PROCESSED):
+  postIndex = []
+  for filename in glob.glob( C_HTMLPATH + "/*." + C_PROCESSED):
+    print filename
+    postIndex.append( {
+      'baseName': os.path.basename(filename),
+      'title': " ".join( os.path.basename(filename).split('.')[0].split('-')[3:] ),
+      'postDate': time.strptime( "%s %s %s" %  tuple(os.path.basename(filename).split('-')[0:3]), "%Y %m %d")
+    })
+  return postIndex
 
+def htmlDateIndex(postIndex):
+  postData = "<table class=postList>"
+  for post in sorted(postIndex, key=lambda p: p['postDate'], reverse=True):
+    postData += "<tr class=post>"
+    postData += "<td class=postDate>" + time.strftime("%Y &bull; %m &bull; %d", post['postDate']) + "</td>"
+    postData += "<td class=postTitle><a href=./html/" + post['baseName'] + ">" + post['title'] + "</a></td>"
+    postData += "</tr>"
+  postData += "</table>"
 
-# stage 2, create an index of all the html files
-postIndex = []
-for filename in glob.glob( C_HTMLPATH + "/*." + C_PROCESSED):
-  postIndex.append( {
-    'baseName': os.path.basename(filename),
-    'title': " ".join( os.path.basename(filename).split('.')[0].split('-')[3:] ),
-    'postDate': time.strptime( "%s %s %s" %  tuple(os.path.basename(filename).split('-')[0:3]), "%Y %m %d")
-  })
+# render all the pages
+if os.path.isfile("makeindex.cfg"):
+  config.read('makeindex.cfg')
 
-# stage 3 create the post list
-postData = "<table class=postList>"
-for post in sorted(postIndex, key=lambda p: p['postDate'], reverse=True):
-  postData += "<tr class=post>"
-  postData += "<td class=postDate>" + time.strftime("%Y &bull; %m &bull; %d", post['postDate']) + "</td>"
-  postData += "<td class=postTitle><a href=./html/" + post['baseName'] + ">" + post['title'] + "</a></td>"
-  postData += "</tr>"
-postData += "</table>"
+  C_DEBUG         = config.getboolean('main','debug')
+  C_INDEXTEMPLATE = config.get('main', 'indextemplate')
+  C_UNPROCESSED   = config.get('main','unprocessed')
+  C_PROCESSED     = config.get('main','processed')
 
-# stage 4 print out the index.html
-with open('index.html', "w") as f:
-  f.write( pystache.render(C_INDEXTEMPLATE, {'content': postData}))
+  # render all html files to give them a header and footer and style
+  for section in config.sections():
+    if section != "main":
+      C_TEMPLATE = open( config.get(section,'template')).read()
+      C_PATH = config.get(section,'htmlPath')
+      C_INDEX = config.get(section,'indextemplate')
+
+      renderPages( C_PATH , C_TEMPLATE, C_UNPROCESSED)
+
+    if section == "index":
+      index = createDateIndex( C_PATH , C_PROCESSED)
+      html = htmlDateIndex(index)
+
+      with open('index.html', "w") as f:
+        f.write( pystache.render(C_INDEXTEMPLATE, {'content': html}))
